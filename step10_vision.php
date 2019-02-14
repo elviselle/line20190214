0daaef43319441396d627a423349b8283cfa222b
Elvis Wang <elviselle@gmail.com>
2019-02-14 16:51:20 +0800

    Update step10_vision.php

--- step10_vision.php
+++ step10_vision.php
@@ -47,7 +47,7 @@
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
     $result = json_decode(curl_exec($ch));
     $result_ary = explode("\n",$result -> responses[0] -> fullTextAnnotation -> text);
-    //fwrite($myfile, "\xEF\xBB\xBF".json_encode($result -> responses[0] -> fullTextAnnotation -> text));
+    fwrite($myfile, "\xEF\xBB\xBF".json_encode($result -> responses[0] -> fullTextAnnotation -> text));
     $ans_txt = "這張發票沒用了，你又製造了一張垃圾";
     foreach ($result_ary as $val) {
         if($val == "MB-76164441"){
